879606c467c44d32a205519bad4acea976a5761f
aangelo21 <martinezglielmo@gmail.com>
2025-04-24 21:48:13 +0100

    Conexión a la base de datos creada

--- src/es/daw/angelo/biblioteca/Biblioteca.java
+++ src/es/daw/angelo/biblioteca/Biblioteca.java
@@ -1,4 +1,11 @@
 package es.daw.angelo.biblioteca;
 
+import es.daw.angelo.biblioteca.config.ConexionDB;
+
+import java.sql.*;
+
 public class Biblioteca {
+    public static void main(String[] args) {
+        Connection conn = ConexionDB.conectar();
+    }
 }
--- src/es/daw/angelo/biblioteca/config/ConexionDB.java
+++ src/es/daw/angelo/biblioteca/config/ConexionDB.java
@@ -1,4 +1,21 @@
 package es.daw.angelo.biblioteca.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionDB {
+    public static Connection conectar() {
+        Connection conn = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:biblioteca.db");
+            System.out.println("Conexión a la base de datos establecida");
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver no encontrado");
+        }
+        return conn;
+    }
 }
